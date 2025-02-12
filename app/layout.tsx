fc2b23817c458729a86a303f776f3e537819553b
Priyanshu-pps007 <priyanshupps2925@gmail.com>
2025-02-13 01:47:24 +0530

    updated-code

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta name="theme-color" content="#111827"/> 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
